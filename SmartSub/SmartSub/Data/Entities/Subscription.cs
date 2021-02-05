03d8a785ac17fb6176b020346a52f1766f19baf3
RussFabre <russel.fabre@selu.edu>
2021-02-05 12:01:55 -0600

    created tables

---
+++ SmartSub/SmartSub/Data/Entities/Subscription.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSub.Data.Entities
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public string Name { get; set;  }
+    }
+}
