7bd43d3da1925bc7844bf1dab8c4614725aaf79e
cody-babin <cody.babin-2@selu.edu>
2021-02-07 15:54:23 -0600

    updating tier table

--- SmartSub/SmartSub/Data/Entities/tier-table.cs
+++ SmartSub/SmartSub/Data/Entities/tier-table.cs
@@ -1,14 +1,20 @@
+using System.Collections.Generic;
+
 namespace SmartSub.Data.Entities
 {
-    public class tier_table {
+    public class general_info {
         
         public int userId { get; set; } // user info
-        
-        public int price { get; set; } // price of the tier 
         public string serviceName { get; set; } // name of the service subbed to
+        public List<tier> info { get; set; }
+        
+    }// end 
+
+    public class tier {
+        
         public string tierName { get; set; } // name of the tier that user has
+        public int price { get; set; } // price of the tier
         public string[] function { get; set; } // shows what comes with this tier of the sub.
         
-        
-    }
+    }// end test 
 }
