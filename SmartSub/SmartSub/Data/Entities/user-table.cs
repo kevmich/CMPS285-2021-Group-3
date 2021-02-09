e07cc46cb9d6b745781c6e8110556fe74e286bd6
Grant Simon <grant.simon-2@selu.edu>
2021-02-09 13:06:16 -0600

    added user table

---
+++ SmartSub/SmartSub/Data/Entities/user-table.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SmartSub.Data.Entities
+public class UserTableDTO
+{
+    public int Id { get; set; }             //user info
+    public string FirstName { get; set; }   //first name
+    public string LastName { get; set; }    //last name
+    public string Username { get; set; }    //username
+    public string Email { get; set; }       //user email
+    public string Role { get; set; }        //???
+}
