4f120fc2cf354e400023dca930bcf7f23083142a
cody-babin <cody.babin-2@selu.edu>
2021-02-15 23:54:19 -0600

    Minor changes and getting started on logout endpoint

--- SmartSub/SmartSub/Controllers/AuthController.cs
+++ SmartSub/SmartSub/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using SmartSub.Data;
 using SmartSub.Data.Entities;
 using SmartSub.Features.User;
@@ -9,19 +12,38 @@ namespace SmartSub.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase 
+    public class AuthController : ControllerBase
     {
 
         private readonly UserManager<User> userManager;
         private readonly SignInManager<User> signInManager;
 
-        
+
         public AuthController(DataContext dataContext, UserManager<User> userManager, RoleManager<Role> roleManager, SignInManager<User> signInManager)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
         }
 
+
+
+        [HttpPost("Create")]
+        public async Task<ActionResult> Create(CreateUserDto dto)
+        {
+            var user = new User { UserName = dto.Username };
+            var result = await userManager.CreateAsync(user, dto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+
+            await userManager.AddToRoleAsync(user, "User");
+
+            // User added successfully, you can safely use the Id now.
+
+            return Ok();
+        }
 
 
         [HttpPost("Login")]
@@ -53,21 +75,9 @@ namespace SmartSub.Controllers
 
 
 
-        [HttpPost("Create")]
-        public async Task<ActionResult> Create(CreateUserDto dto)
+     [HttpPost("Logout")]
+       public async Task<ActionResult> Logout()
         {
-            var user = new User {UserName = dto.Username};
-            var result = await userManager.CreateAsync(user, dto.Password);
-
-            if (!result.Succeeded)
-            {
-                return BadRequest();
-            }
-            
-            await userManager.AddToRoleAsync(user, "User");
-
-            // User added successfully, you can safely use the Id now.
-            
             return Ok();
         }
     }
