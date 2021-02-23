2629f17d407f5af282bac0f6f1bbf5d2870b269b
Alan <alan.schmidt@selu.edu>
2021-02-23 14:55:34 -0600

    Frontend description

--- SmartSub/SmartSub/ClientApp/smartsub/src/Pages/HomePage.js
+++ SmartSub/SmartSub/ClientApp/smartsub/src/Pages/HomePage.js
@@ -3,10 +3,14 @@ import NavBar from "../Components/NavBar/NavBar";
 function HomePage(){
     return(
         <div>
-            <NavBar /> Description of the site
-            
-
-
+            <NavBar />
+               <p style={{fontSize: "29px"}}> Smartsub is a website designed to help people manage their subscriptions. The average number of
+                services people are subscribed to is becoming far too cumbersome for any one person to reasonably manage.
+                Smartsub allows for a very simple user interface that creates and easy, streamlined experience for any user
+                to add a subscription to their personalized list, manage the price of each sub, and view a calender showing
+                their upcoming charges for the next month. If you are a new user, please click the
+                <a style={{color: "blue"}}> sign up </a>
+                button to begin simplifying the management of your subscriptions!</p>
         </div>
     )
 }
