0ec05f1cb765bd7b9a9e3f4ad1cc9c7da5455a28
RussFabre <russel.fabre@selu.edu>
2021-03-04 21:52:37 -0600

    axios user table source code added, not functional
    missing endpt for table data

--- SmartSub/SmartSub/ClientApp/smartsub/src/Pages/UserPage/UserPage.js
+++ SmartSub/SmartSub/ClientApp/smartsub/src/Pages/UserPage/UserPage.js
@@ -1,13 +1,8 @@
 import NavBar from "../../Components/NavBar/NavBar";
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React, {useEffect, useState} from 'react';
+import { makeStyles, Paper, TableCell, Table, TableHead, TableBody,
+    TableContainer, TableRow } from '@material-ui/core';
+import axios from "axios";
 
 const useStyles =makeStyles({
     table: {
@@ -15,50 +10,122 @@ const useStyles =makeStyles({
     }
 });
 
+const getData = [
 
+];
 
-
-function createData(Provider, RenewDate, Note, Price, paymentFreq){
-    return { Provider, RenewDate, Note, Price, paymentFreq}
+/*function getUser() {
+    return axios.get('/user?ID=12345')
+        .then(function (response){
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function() {
+        })
+}
+function getProvider() {
+    return axios.get(Provider)
+        .then(function (response){
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function() {
+        })
+}
+function getRenewDate() {
+    return axios.get(RenewDate)
+        .then(function (response){
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function() {
+        })
+}
+function getPrice() {
+    return axios.get(Price)
+        .then(function (response){
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function() {
+        })
+}
+function getPaymentFrequency() {
+    return axios.get(PaymentFrequency)
+        .then(function (response){
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function() {
+        })
 }
 
-const rows = [
-    createData("Netflix", "Jan 1st, 2021", "no commercials", '$11.99', "Monthly"),
-    createData("Hulu", "Jan 1st, 2021", "commercials", '$5.99', "Monthly"),
-];
+ */
+
+function createData(Provider, RenewDate, Price, PaymentFrequency){
+    return {Provider, RenewDate, Price, PaymentFrequency}
+}
 
 export default function UserPage() {
     const classes = useStyles();
+    const [tableInfo, setTableInfo] = useState({
+        col: [{
+            provider: "Provider",
+            renewDate: "Renew Date",
+            price: "Price",
+            paymentFrequency: "Payment Frequency"
+        }],
+        info: []
+    });
+    useEffect(() => {
+        axios.get('')
+            .then((response) => {
+                setTableInfo((table) => {
+                    const infoCall = {...table};
+                    response.data.map((d)=>{
+                        infoCall.data = [...infoCall.data, d];
+                    })
+                    return infoCall;
+                })
+            })
+    },[])
+    const rows = tableInfo.info;
 
     return (
         <div>
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Provider</TableCell>
-                        <TableCell align="right">Renew Date</TableCell>
-                        <TableCell align="right">Note</TableCell>
-                        <TableCell align="right">Price</TableCell>
-                        <TableCell align="right">Frequency</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.Provider}>
-                            <TableCell component="th" scope="row">
-                                {row.Provider}
-                            </TableCell>
-                            <TableCell align="right">{row.RenewDate}</TableCell>
-                            <TableCell align="right">{row.Note}</TableCell>
-                            <TableCell align="right">{row.Price}</TableCell>
-                            <TableCell align="right">{row.paymentFreq}</TableCell>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Provider</TableCell>
+                            <TableCell align="right">Renew Date</TableCell>
+                            <TableCell align="right">Price</TableCell>
+                            <TableCell align="right">Frequency</TableCell>
                         </TableRow>
+                    </TableHead>
+                    {rows.map((row) => (
+                        <TableBody>
+                            <TableRow key={row.provider}>
+                                <th component="th" scope="row">{row.provider}</th>
+                                <th align="right">{row.renewDate}</th>
+                                <th align="right">{row.price}</th>
+                                <th align="right">{row.paymentFrequency}</th>
+                            </TableRow>
+                        </TableBody>
                     ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-            </div>
+                </Table>
+            </TableContainer>
+        </div>
     );
 }
 
