302f8833a060732d33509b7c825917a4634a9750
Grant Simon <grant.simon-2@selu.edu>
2021-04-20 13:56:06 -0500

    Experimental User Page

--- SmartSub/SmartSub/ClientApp/smartsub/src/Pages/UserPage/UserPage.js
+++ SmartSub/SmartSub/ClientApp/smartsub/src/Pages/UserPage/UserPage.js
@@ -1,133 +1,177 @@
 import NavBar from "../../Components/NavBar/NavBar";
-import React, {useEffect, useState} from 'react';
-import { makeStyles, Paper, TableCell, Table, TableHead, TableBody,
-    TableContainer, TableRow } from '@material-ui/core';
-import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Input from "@material-ui/core/Input";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
 
-const useStyles =makeStyles({
+import EditIcon from "@material-ui/icons/EditOutlined";
+import DoneIcon from "@material-ui/icons/DoneAllTwoTone";
+import RevertIcon from "@material-ui/icons/NotInterestedOutlined";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: "100%",
+        marginTop: theme.spacing(3),
+        overflowX: "auto"
+    },
     table: {
-        minWidth: 650,
+        minWidth: 650
+    },
+    selectTableCell: {
+        width: 60
+    },
+    tableCell: {
+        width: 130,
+        height: 40
+    },
+    input: {
+        width: 130,
+        height: 40
     }
+}));
+
+const createData = (name, renewDate, price, paymentFrequency) => ({
+    id: name.replace(" ", "_"),
+    name,
+    renewDate,
+    price,
+    paymentFrequency,
+    isEditMode: false
 });
 
-const getData = [
-
-];
-
-/*function getUser() {
-    return axios.get('/user?ID=12345')
-        .then(function (response){
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .then(function() {
-        })
-}
-function getProvider() {
-    return axios.get(Provider)
-        .then(function (response){
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .then(function() {
-        })
-}
-function getRenewDate() {
-    return axios.get(RenewDate)
-        .then(function (response){
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .then(function() {
-        })
-}
-function getPrice() {
-    return axios.get(Price)
-        .then(function (response){
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .then(function() {
-        })
-}
-function getPaymentFrequency() {
-    return axios.get(PaymentFrequency)
-        .then(function (response){
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .then(function() {
-        })
-}
-
- */
-
-function createData(Provider, RenewDate, Price, PaymentFrequency){
-    return {Provider, RenewDate, Price, PaymentFrequency}
-}
-
-export default function UserPage() {
+const CustomTableCell = ({ row, name, onChange }) => {
     const classes = useStyles();
-    const [tableInfo, setTableInfo] = useState({
-        col: [{
-            provider: "Provider",
-            renewDate: "Renew Date",
-            price: "Price",
-            paymentFrequency: "Payment Frequency"
-        }],
-        info: []
-    });
-    useEffect(() => {
-        axios.get('')
-            .then((response) => {
-                setTableInfo((table) => {
-                    const infoCall = {...table};
-                    response.data.map((d)=>{
-                        infoCall.data = [...infoCall.data, d];
-                    })
-                    return infoCall;
-                })
-            })
-    },[])
-    const rows = tableInfo.info;
+    const { isEditMode } = row;
+    return (
+        <TableCell align="left" className={classes.tableCell}>
+            {isEditMode ? (
+                <Input
+                    value={row[name]}
+                    name={name}
+                    onChange={e => onChange(e, row)}
+                    className={classes.input}
+                />
+            ) : (
+                row[name]
+            )}
+        </TableCell>
+    );
+};
+
+export default function App() {
+    const [rows, setRows] = React.useState([
+        createData("NA", 0, 0, 0),
+        createData("NA", 0, 0, 0),
+        createData("NA", 0, 0, 0)
+    ]);
+    const [previous, setPrevious] = React.useState({});
+    const classes = useStyles();
+
+    const onToggleEditMode = id => {
+        setRows(state => {
+            return rows.map(row => {
+                if (row.id === id) {
+                    return { ...row, isEditMode: !row.isEditMode };
+                }
+                return row;
+            });
+        });
+    };
+
+    const onChange = (e, row) => {
+        if (!previous[row.id]) {
+            setPrevious(state => ({ ...state, [row.id]: row }));
+        }
+        const value = e.target.value;
+        const name = e.target.name;
+        const { id } = row;
+        const newRows = rows.map(row => {
+            if (row.id === id) {
+                return { ...row, [name]: value };
+            }
+            return row;
+        });
+        setRows(newRows);
+    };
+
+    const onRevert = id => {
+        const newRows = rows.map(row => {
+            if (row.id === id) {
+                return previous[id] ? previous[id] : row;
+            }
+            return row;
+        });
+        setRows(newRows);
+        setPrevious(state => {
+            delete state[id];
+            return state;
+        });
+        onToggleEditMode(id);
+    };
 
     return (
-        <div>
-            <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Provider</TableCell>
-                            <TableCell align="right">Renew Date</TableCell>
-                            <TableCell align="right">Price</TableCell>
-                            <TableCell align="right">Frequency</TableCell>
+        <Paper className={classes.root}>
+            <Table className={classes.table} aria-label="caption table">
+                <caption style={{textAlign: "center", textTransform: "Uppercase", color: "black",}}>Use the data table to manage all of your subscriptions!</caption>
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="left" />
+                        <TableCell style={{textAlign: "left", textTransform: "Uppercase", color: "black",}}>Subscription Provider</TableCell>
+                        <TableCell style={{textAlign: "left", textTransform: "Uppercase", color: "black",}}>Renew Date</TableCell>
+                        <TableCell style={{textAlign: "left", textTransform: "Uppercase", color: "black",}}>Price</TableCell>
+                        <TableCell style={{textAlign: "left", textTransform: "Uppercase", color: "black",}}>Payment Frequency</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map(row => (
+                        <TableRow key={row.id}>
+                            <TableCell className={classes.selectTableCell}>
+                                {row.isEditMode ? (
+                                    <>
+                                        <IconButton
+                                            aria-label="done"
+                                            onClick={() => onToggleEditMode(row.id)}
+                                        >
+                                            <DoneIcon />
+                                        </IconButton>
+                                        <IconButton
+                                            aria-label="revert"
+                                            onClick={() => onRevert(row.id)}
+                                        >
+                                            <RevertIcon />
+                                        </IconButton>
+                                    </>
+                                ) : (
+                                    <IconButton
+                                        aria-label="delete"
+                                        onClick={() => onToggleEditMode(row.id)}
+                                    >
+                                        <EditIcon />
+                                    </IconButton>
+                                )}
+                            </TableCell>
+                            <CustomTableCell {...{ row, name: "name", onChange }} />
+                            <CustomTableCell {...{ row, name: "renewDate", onChange }} />
+                            <CustomTableCell {...{ row, name: "price", onChange }} />
+                            <CustomTableCell {...{ row, name: "paymentFrequency", onChange }} />
                         </TableRow>
-                    </TableHead>
-                    {rows.map((row) => (
-                        <TableBody>
-                            <TableRow key={row.provider}>
-                                <th component="th" scope="row">{row.provider}</th>
-                                <th align="right">{row.renewDate}</th>
-                                <th align="right">{row.price}</th>
-                                <th align="right">{row.paymentFrequency}</th>
-                            </TableRow>
-                        </TableBody>
                     ))}
-                </Table>
-            </TableContainer>
-        </div>
+                </TableBody>
+            </Table>
+        </Paper>
     );
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
 
 
 
