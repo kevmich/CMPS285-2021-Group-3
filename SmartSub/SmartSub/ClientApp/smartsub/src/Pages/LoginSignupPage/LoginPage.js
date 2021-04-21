f52c70de5659821050ad1431f60a0e2d6d25bdb5
cody-babin <cody.babin-2@selu.edu>
2021-04-21 14:10:40 -0500

    Creating Login redirect to user page

--- SmartSub/SmartSub/ClientApp/smartsub/src/Pages/LoginSignupPage/LoginPage.js
+++ SmartSub/SmartSub/ClientApp/smartsub/src/Pages/LoginSignupPage/LoginPage.js
@@ -25,36 +25,6 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-// let LoginAxiosCall = (username, password) => {
-//     console.log(username, password);
-//         axios({
-//             method: 'post',
-//             url: '/Auth/Login',
-//             data: {
-//                 userName: username,
-//                 passWord: password
-//             }
-//         })
-//         .catch(function (error) {
-//             if (error.response) {
-//                 // The request was made and the server responded with a status code
-//                 // that falls out of the range of 2xx
-//                 console.log(error.response.data);
-//                 console.log(error.response.status);
-//                 console.log(error.response.headers);
-//             } else if (error.request) {
-//                 // The request was made but no response was received
-//                 // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-//                 // http.ClientRequest in node.js
-//                 console.log(error.request);
-//             } else {
-//                 // Something happened in setting up the request that triggered an Error
-//                 console.log('Error', error.message);
-//             }
-//             console.log(error.config);
-//         })
-//     }
-
 export default function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
