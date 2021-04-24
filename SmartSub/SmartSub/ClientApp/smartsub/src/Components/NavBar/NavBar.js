215707c00e228ca0d8c94ebfb4ed41de8762b957
cody-babin <cody.babin-2@selu.edu>
2021-04-24 15:07:43 -0500

    minor change

--- SmartSub/SmartSub/ClientApp/smartsub/src/Components/NavBar/NavBar.js
+++ SmartSub/SmartSub/ClientApp/smartsub/src/Components/NavBar/NavBar.js
@@ -33,7 +33,7 @@ const navLinks = [
     { title: `user page`, path: `/UserPage` },
     { title: `login`, path: `/LoginPage` },
     { title: `sign up`, path: `/SignupPage` },
-    { title: `log out  `, path: `/LogOut`}
+    { title: `log out  `, path: `/`}
 ];
 
 const NavBar = () => {
