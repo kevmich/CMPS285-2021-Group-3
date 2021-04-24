69b18bebc645b0973c3822d14b29f08169ad2d78
Grant Simon <grant.simon-2@selu.edu>
2021-04-24 18:19:56 -0500

    More UserTable Adjustments

--- SmartSub/SmartSub/ClientApp/smartsub/src/Pages/UserPage/UserPage.js
+++ SmartSub/SmartSub/ClientApp/smartsub/src/Pages/UserPage/UserPage.js
@@ -1,14 +1,19 @@
-import NavBar from "../../Components/NavBar/NavBar";
 import React, {useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import axios from "axios";
 
 const columns = [
@@ -33,38 +38,77 @@ const columns = [
         minWidth: 100,
         align: 'center',
     },
-    {
-        id: 'note',
-        label: 'Note',
-        minWidth: 100,
-        align: 'right',
-    }
 ];
 
-
-const useStyles = makeStyles({
+const useRowStyles = makeStyles({
     root: {
-        width: '100%',
-    },
-    container: {
-        maxHeight: 470,
+        '& > *': {
+            borderBottom: 'unset',
+        },
     },
 });
 
-export default function StickyHeadTable() {
-    const classes = useStyles();
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+function Row(props) {
+    const { row } = props;
+    const [open, setOpen] = React.useState(false);
+    const classes = useRowStyles();
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
+    return (
+        <React.Fragment>
+            <TableRow className={classes.root}>
+                <TableCell>
+                    <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    {row.name}
+                </TableCell>
+                <TableCell >{row.provider}</TableCell>
+                <TableCell >{row.price}</TableCell>
+                <TableCell >{row.paymentFrequency}</TableCell>
+                <TableCell >{row.renewDate}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box margin={1}>
+                            <Typography variant="h6" gutterBottom component="div">
+                                {row.note}
+                            </Typography>
+                            <Table size="small" aria-label="purchases">
+                                <TableHead>
+                                </TableHead>
+                                <TableBody>
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
+}
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
+Row.propTypes = {
+    row: PropTypes.shape({
+        calories: PropTypes.number.isRequired,
+        carbs: PropTypes.number.isRequired,
+        fat: PropTypes.number.isRequired,
+        history: PropTypes.arrayOf(
+            PropTypes.shape({
+                amount: PropTypes.number.isRequired,
+                customerId: PropTypes.string.isRequired,
+                date: PropTypes.string.isRequired,
+            }),
+        ).isRequired,
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        protein: PropTypes.number.isRequired,
+    }).isRequired,
+};
 
+export default function CollapsibleTable() {
     const [tableInfo, setTableInfo] = useState({
         col: [{
             id: "Id",
@@ -72,7 +116,6 @@ export default function StickyHeadTable() {
             price: "Price",
             paymentFrequency: "PaymentFrequency",
             renewDate: "RenewDate",
-            note: "Note",
         }],
         info: [],
     });
@@ -90,46 +133,29 @@ export default function StickyHeadTable() {
     },[])
     const rows = tableInfo.info;
 
-
     return (
-        <Paper className={classes.root}>
-            <TableContainer className={classes.container}>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            {columns.map((column) => (
-                                <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    style={{ minWidth: column.minWidth }}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
+        <TableContainer component={Paper}>
+            <Table aria-label="collapsible table">
+
+                <TableHead>
+                    <TableRow>
+                        {columns.map((column) => (
+                            <TableCell
+                                key={column.id}
+                                align={column.align}
+                                style={{ minWidth: column.minWidth }}
+                            >
+                                {column.label}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
                     {rows.map((row) => (
-                        <TableBody>
-                            <TableRow key={row.provider}>
-                                <th align="left" component="th" scope="row">{row.provider}</th>
-                                <th align="center">{row.price}</th>
-                                <th align="center">{row.paymentFrequency}</th>
-                                <th align="center">{row.renewDate}</th>
-                                <th align="right">{row.note}</th>
-                            </TableRow>
-                        </TableBody>
+                        <Row key={row.name} row={row} />
                     ))}
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-            />
-        </Paper>
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
