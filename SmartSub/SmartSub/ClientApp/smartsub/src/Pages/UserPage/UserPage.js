c2ec805404acc45af787357d6e1a589e6b2230ae
cody-babin <cody.babin-2@selu.edu>
2021-05-01 02:26:41 -0500

    Minor changes

--- SmartSub/SmartSub/ClientApp/smartsub/src/Pages/UserPage/UserPage.js
+++ SmartSub/SmartSub/ClientApp/smartsub/src/Pages/UserPage/UserPage.js
@@ -130,6 +130,7 @@ function Row(props) {
                 <TableCell >{row.price}</TableCell>
                 <TableCell >{row.paymentFrequency}</TableCell>
                 <TableCell >{moment(row.renewDate).format('MMM Do YYYY')}</TableCell>
+        
             </TableRow>
             <TableRow>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0, background: "white" }} colSpan={6}>
